c160901c740d9a96d5c573aa3f6e7297b94563f8
Timothy Curchod <timofeyc@hotmail.com>
2015-05-07 09:46:57 +0000

    Deleted the Angular app and got livereload and file watch working

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,23 @@
 var gulp = require('gulp');
-
-var EXPRESS_PORT = 4000;
-var EXPRESS_ROOT = '.';
-var LIVERELOAD_PORT = 35729;
-
-function startExpress() {
-  var express = require('express');
-  var app = express();
-  app.use(require('connect-livereload')());
-  app.use(express.static(EXPRESS_ROOT));
-  app.listen(EXPRESS_PORT);
-}
+var connect = require('gulp-connect');
+var jshint = require('gulp-jshint');
+var server = require('gulp-develop-server');
  
-var lr;
-function startLivereload() {
-  lr = require('tiny-lr')();
-  lr.listen(LIVERELOAD_PORT);
-  // go to http://localhost:35729 and 
-  //you'll get a warm JSON welcome from tiny-lr.
-}
-
-// Notifies livereload of changes detected by gulp watch 
-function notifyLivereload(event) {
-  // `gulp.watch()` events provide an absolute path
-  // so we need to make it relative to the server root
-  var fileName = require('path').relative(EXPRESS_ROOT, event.path);
-  lr.changed({
-    body: {
-      files: [fileName]
-    }
-  });
-}
-
-gulp.task('default', function () {
-  startExpress();
-  startLivereload();
-  gulp.watch('*.js', notifyLivereload);
-  console.log('Gulp and running!')
+ // run server 
+gulp.task( 'server:start', function() {
+    server.listen( { path: './server.js' } );
 });
+ 
+// restart server if app.js changed 
+gulp.task( 'server:restart', function() {
+    gulp.watch( [ './server.js' ], server.restart );
+});
+
+gulp.task('lint', function () {
+  gulp.src('./**/*.js')
+    .pipe(jshint())
+})
+
+gulp.task('default', ['server:start', 'server:restart', 'lint'],
+  console.log('Gulp and running!')
+);
